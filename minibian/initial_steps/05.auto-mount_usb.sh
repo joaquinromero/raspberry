4579f21bebbdaf66888121d9e961525c3d235b6f
joaquinromero <joaquin.romero@gmail.com>
2017-11-11 22:11:58 +0100

    Update 05.auto-mount_usb.sh

--- minibian/initial_steps/05.auto-mount_usb.sh
+++ minibian/initial_steps/05.auto-mount_usb.sh
@@ -3,23 +3,46 @@
 #Install udisks-glue to automount usb drives without starting the desktop.
 # install udisks-glue
 apt-get install udisks-glue policykit-1
-# edit config file to enable automount
-sed -i '/^match disks /a\    automount = true' /etc/udisks-glue.conf
 
-#Supervisor provides a simple way to add user services, without messing with SysV or other init scripts.
-apt-get install supervisor
+#add system user with no login
+useradd -r mountgr
 
-nano /etc/supervisor/conf.d/udisks-glue.conf:
---------------------------------------------------------
-[program:udisks-glue]
-user = pi
-command = udisks-glue -f
-autostart = true
-autorestart = true
-stdout_logfile = /var/log/supervisor/udisks-glue-out.log
-stderr_logfile = /var/log/supervisor/udisks-glue-err.log
---------------------------------------------------------
-# reload supervisor config
-supervisorctl reload
+nano /etc/systemd/system/udisks-glue.service
+#-------------------------------------------------------------
+[Unit]
+Description = mount disks automatically with standby
+After = local-fs.target
 
-#The udisks-glue program should start automatically on boot. Use supervisorctl with start, stop, and restart to manually control Supervisor programs: supervisorctl restart udisks-glue.
+[Service]
+User = jok
+Group = mountgr
+Type = simple
+#ExecStartPre = /bin/bash -c "rmdir /media/*/ 2>/dev/null; time udevadm settle; exit 0"
+ExecStart = /usr/bin/udisks-glue -c /etc/udisks-glue.conf --foreground
+
+[Install]
+WantedBy = multi-user.target
+#-------------------------------------------------------------
+nano /etc/polkit-1/localauthority/50-local.d/50-mount-as-mountgr.pkla
+#-------------------------------------------------------------
+[Media mounting by mountgr]
+Identity=unix-group:mountgr
+Action=org.freedesktop.udisks.filesystem-mount
+ResultAny=yes
+#-------------------------------------------------------------
+nano /etc/udisks-glue.conf
+#-------------------------------------------------------------
+filter disks {
+    optical = false
+    partition_table = false
+    usage = filesystem
+}
+
+match disks {
+    automount = true
+#    automount_options = {sync,umask=000}
+    post_insertion_command = "udisks --mount %device_file --mount-options sync,umask=007"
+}
+#-------------------------------------------------------------
+#añadir usuarios al grupo
+usermod -a -G mountgr <user>
